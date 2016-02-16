e11e2f762fd95f5668677fad4ed0fccd987941de
Tom Kidd <tomjkidd@gmail.com>
2016-02-16 08:33:36 -0500

    Adding Week4/Day2 of livining-clojure training-plan

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -27,32 +27,55 @@
 (defn table
   "The alphabet based table for encode/decode"
   []
-  (loop [acc {} remaining alphabet]
+  (loop [acc [] remaining alphabet]
     (let [f (first remaining)
           val (if (nil? f) nil (rotate (char-index f) alphabet))
           r (rest remaining)]
       (if (nil? f)
         acc
-        (recur (assoc acc f val) r)))))
+        (recur (conj acc val) r)))))
 
-(defn lookup
+(defn column
+  "Get a column from the alphabet table"
+  [n]
+  (->> (table)
+       (map #(nth %  n))))
+
+(defn encode-letter
   [row-char col-char]
-  (nth ((table) row-char) (char-index col-char)))
+  (-> (table)
+       (nth (char-index row-char))
+       (nth (char-index col-char))))
 
 (defn match-length
   [n message]
   (take n (cycle message)))
+
+(defn decode-letter
+  [key-char enc-char]
+  (let [decoded-index (->> (column (char-index key-char))
+                           (map-indexed (fn [idx itm] [idx itm]))
+                           (filter #(= (first (rest %)) enc-char))
+                           (first)
+                           (first))
+        decoded-char (nth (nth (table) 0) decoded-index)]
+    decoded-char))
 
 (defn encode [keyword message]
   (let [to-take (max (count keyword) (count message))
         k (match-length to-take keyword)
         m (match-length to-take message)
         zipper (map vector k m)]
-    (->> (map #(lookup (first %) (first (rest %))) zipper)
-        (reduce str ""))))
+    (->> (map #(encode-letter (first %) (first (rest %))) zipper)
+         (reduce str ""))))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn decode [keyword encoded]
+  (let [to-take (max (count keyword) (count encoded))
+        k (match-length to-take keyword)
+        e (match-length to-take encoded)
+        zipper (map vector k e)]
+    (->> (map #(decode-letter (first %) (first (rest %))) zipper)
+         (reduce str ""))))
 
 (defn decipher [cipher message]
   "decypherme")
