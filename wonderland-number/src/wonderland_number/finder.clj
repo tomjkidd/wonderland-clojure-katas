2b0138f28a92ab74c9f0e4a88ed77694ffd2591b
Tom Kidd <tomjkidd@gmail.com>
2016-02-17 08:52:15 -0500

    Adding Week4/Day2 of living-clojure training-plan.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,24 @@
 (ns wonderland-number.finder)
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  "A number that retains its digits when multiplied by 2,3,4,5, or 6.
+The number has 6 digits."
+  142857)
+
+(defn hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(defn find-wonderland-number
+  "Use the definition"
+  []
+  (loop [wondernum 1]
+    (if (and (= 6 (count (str wondernum)))
+             (hasAllTheSameDigits? wondernum (* 2 wondernum))
+             (hasAllTheSameDigits? wondernum (* 3 wondernum))
+             (hasAllTheSameDigits? wondernum (* 4 wondernum))
+             (hasAllTheSameDigits? wondernum (* 5 wondernum))
+             (hasAllTheSameDigits? wondernum (* 6 wondernum)))
+      wondernum
+      (recur (inc wondernum)))))
--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -2,11 +2,6 @@
   (:require [clojure.test :refer :all]
             [wonderland-number.finder :refer :all]))
 
-(defn hasAllTheSameDigits? [n1 n2]
-  (let [s1 (set (str n1))
-        s2 (set (str n2))]
-    (= s1 s2)))
-
 (deftest test-wonderland-number
   (testing "A wonderland number must have the following things true about it"
     (let [wondernum (wonderland-number)]
