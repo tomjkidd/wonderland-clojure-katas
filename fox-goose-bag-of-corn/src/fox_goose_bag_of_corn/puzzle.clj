e5399a3b034809ee45c5af9dffd6eb02fdb50133
Tom Kidd <tomjkidd@gmail.com>
2016-02-19 12:22:21 -0500

    Adding Week4/Day5 of living-clojure training-plan.

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,22 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+(def plan
+  [[[:fox :goose :corn :you] [:boat] []]
+   [[:fox :corn] [:boat :you :goose] []]
+   [[:fox :corn] [:boat] [:you :goose]]
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:corn] [:boat :you :fox] [:goose]]
+   [[:corn] [:boat] [:you :fox :goose]]
+   [[:corn] [:boat :you :goose] [:fox]]
+   [[:corn :you :goose] [:boat] [:fox]]
+   [[:goose] [:boat :you :corn] [:fox]]
+   [[:goose] [:boat] [:you :fox :corn]]
+   [[:goose] [:boat :you] [:fox :corn]]
+   [[:goose :you] [:boat] [:fox :corn]]
+   [[] [:boat :you :goose] [:fox :corn]]
+   [[] [:boat] [:you :goose :fox :corn]]])
+
 (defn river-crossing-plan []
-  start-pos)
+  plan)
