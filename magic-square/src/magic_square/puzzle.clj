505ea9c52ed6b35eda97a254e9fb3012d8f0b08f
Tom Kidd <tomjkidd@gmail.com>
2016-02-26 17:46:19 -0500

    Adding Week5/Day5 of living-clojure training-plan.

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,4 +1,5 @@
-(ns magic-square.puzzle)
+(ns magic-square.puzzle
+  (:require [clojure.math.combinatorics :as combo]))
 
 (def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
@@ -23,8 +24,45 @@
      (+ a e i)
      (+ c e g)))
 
-(defn magic-square?
+(defn magic-square-inefficient?
   "Determine if the proposed matrix is a magic square."
   [matrix]
   (let [sums (magic-square-sums matrix)]
     (every? #(= (first sums) %) sums)))
+
+(defn magic-square?
+  [matrix]
+  (let [[[a b c]
+         [d e f]
+         [g h i]] matrix]
+    (if (= (+ a b c)
+           (+ d e f)
+           (+ g h i)
+           (+ a d g)
+           (+ b e h)
+           (+ c f i)
+           (+ a e i)
+           (+ c e g))
+      matrix
+      false)))
+
+(defn vec-to-matrix [[a b c d e f g h i]] [[a b c] [d e f] [g h i]])
+
+(defn find-magic-squares-with
+  [func]
+  (let [ps (->> values
+                (combo/permutations)
+                (map vec-to-matrix))]
+    (func magic-square? ps)))
+
+(defn find-magic-square
+  []
+  (find-magic-squares-with some))
+
+(defn find-all-magic-squares
+  []
+  (find-magic-squares-with filter))
+
+(defn print-square
+  [rows]
+  (map prn rows))
