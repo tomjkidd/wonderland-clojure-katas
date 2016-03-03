59f6b6eba8282811a6dd2a35bd492daf11010320
Tom Kidd <tomjkidd@gmail.com>
2016-03-03 16:51:04 -0500

    Adding the round-level unit tests for war game.

--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -1,18 +1,29 @@
 (ns card-game-war.game-test
   (:require [clojure.test :refer :all]
-            [card-game-war.game :refer :all]))
+            [card-game-war.game2 :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= (winner [[:spade 2] [:spade 3]])
+           [:spade 3])))
+  (testing "queens are higher rank than jacks"
+    (is (= (winner [[:spade :queen] [:spade :jack]])
+           [:spade :queen])))
+  (testing "kings are higher rank than queens"
+    (is (= (winner [[:spade :king] [:spade :queen]])
+           [:spade :king])))
+  (testing "aces are higher rank than kings"
+    (is (= (winner [[:spade :ace] [:spade :king]])
+           [:spade :ace])))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= (winner [[:club 2] [:spade 2]])
+           [:club 2])))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= (winner [[:diamond 2] [:club 2]])
+           [:diamond 2])))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= (winner [[:heart 2] [:diamond 2]])
+           [:heart 2]))))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
